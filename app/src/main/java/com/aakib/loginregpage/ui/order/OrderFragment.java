f6120c6edbc27e1e6513c5e32a7894f881334162
aakib <aakib.khan@girmiti.com>
2021-01-27 22:20:37 +0530

    commit text

--- app/src/main/java/com/aakib/loginregpage/ui/order/OrderFragment.java
+++ app/src/main/java/com/aakib/loginregpage/ui/order/OrderFragment.java
@@ -16,15 +16,15 @@ import com.aakib.loginregpage.R;
 
 public class OrderFragment extends Fragment {
 
-    private OrderViewModel slideshowViewModel;
+    private OrderViewModel orderViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
+        orderViewModel =
                 ViewModelProviders.of(this).get(OrderViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
         final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        orderViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
